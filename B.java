99da7483f6952f85a21f5b42e95c857f2854fc7e
user2 <yar@DESKTOP-MASNESC.localdomain>
2026-03-30 20:57:37 +0300

    r3

--- B.java
+++ B.java
@@ -35,4 +35,8 @@ public class B implements H, J {
     public String nn() {
         "".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.;
     }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
 }
