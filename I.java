be8821933ccfb267462462ea67e635f5727835b0
user2 <yar@DESKTOP-MASNESC.localdomain>
2026-03-30 20:57:37 +0300

    r12

--- I.java
+++ I.java
@@ -11,4 +11,24 @@ public class I extends B {
     public double ee() {
         return 100.500;
     }
+
+    public double ad() {
+        return 11.09;
+    }
+
+    public float ff() {
+        return 0;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public int af() {
+        return -1;
+    }
 }
